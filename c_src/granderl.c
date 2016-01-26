6552111f327276c593583ff3ae80c6046380fb5b
Louis-Philippe Gauthier <lpgauth@gmail.com>
2016-01-26 16:13:22 -0500

    Add upgrade function

--- c_src/granderl.c
+++ c_src/granderl.c
@@ -164,6 +164,14 @@ static int on_load(ErlNifEnv *env __attribute__((unused)),
     return 0;
 }
 
+static int upgrade(ErlNifEnv* env __attribute__((unused)),
+                   void ** priv __attribute__((unused)),
+                   void **old_priv __attribute__((unused)),
+                   ERL_NIF_TERM load_info __attribute__((unused)))
+{
+    return 0;
+}
+
 
 static ErlNifFunc nif_functions[] = {
     {"rdrand", 1, rdrand, 0},
@@ -173,4 +181,4 @@ static ErlNifFunc nif_functions[] = {
     {"rdtsc_mod", 1, rdtsc_mod, 0}
 };
 
-ERL_NIF_INIT(granderl, nif_functions, &on_load, NULL, NULL, NULL)
+ERL_NIF_INIT(granderl, nif_functions, &on_load, NULL, &upgrade, NULL)
