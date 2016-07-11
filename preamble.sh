30da61d7e0371868c0cb018260fc98e613e39cdc
Julian Squires <julian@cipht.net>
2016-07-11 11:23:28 -0400

    Add a test for RDRAND
    We could check proc, but the most portable way is just to compile the
    instruction and see if we get SIGILL when we hit it.

--- preamble.sh
+++ preamble.sh
@@ -10,6 +10,17 @@ if [ "$(uname -s)" = Darwin ]; then
     LDFLAGS="$LDFLAGS -flat_namespace -undefined suppress"
 fi
 
-SRC=c_src
-BUILD=build
-IMPLEMENTATIONS="rdrand xorshift pcg32"
+SRC=./c_src
+BUILD=./build
+IMPLEMENTATIONS="xorshift pcg32"
+
+mkdir -p $BUILD
+
+test_for_rdrand() {
+    "$CC" -o $BUILD/test_for_rdrand $SRC/test_for_rdrand.c && $BUILD/test_for_rdrand
+}
+
+if test_for_rdrand; then
+    CFLAGS="$CFLAGS -DHAVE_RDRAND"
+    IMPLEMENTATIONS="$IMPLEMENTATIONS rdrand"
+fi
