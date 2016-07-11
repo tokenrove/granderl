3d91160ea72d69c87815a69c17bba0a83a3868a2
Julian Squires <julian@cipht.net>
2016-07-11 12:42:46 -0400

    Protect important CFLAGS
    Because Gentoo.

--- preamble.sh
+++ preamble.sh
@@ -2,9 +2,9 @@ set -eu -o pipefail
 
 ERTS_INCLUDE_DIR=${ERTS_INCLUDE_DIR:-$(erl -noshell -s init stop -eval "io:format(\"~s/erts-~s/include/\", [code:root_dir(), erlang:system_info(version)]).")}
 CC=${CC:-cc}
-CFLAGS=${CFLAGS:--fPIC -I${ERTS_INCLUDE_DIR} \
-  -std=gnu11 -O3 -march=native -mtune=native \
-  -ggdb -Wall -Wextra -Wno-missing-field-initializers}
+CFLAGS="-fPIC -I${ERTS_INCLUDE_DIR} -std=gnu11 \
+  -Wall -Wextra -Wno-missing-field-initializers \
+  ${CFLAGS:--O3 -march=native -mtune=native -ggdb}"
 LDFLAGS=${LDFLAGS:-}
 if [ "$(uname -s)" = Darwin ]; then
     LDFLAGS="$LDFLAGS -flat_namespace -undefined suppress"
