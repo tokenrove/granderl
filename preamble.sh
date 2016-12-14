1b9187269ed52a80d1cb24a6b169d2e94cb6d962
Julian Squires <julian@cipht.net>
2016-12-14 16:42:57 -0500

    Fix build when CFLAGS does not contain -O
    It turns out that gcc doesn't inline functions if one doesn't specify at
    least -O, which yields an unlinkable SO at the end.

--- preamble.sh
+++ preamble.sh
@@ -4,7 +4,7 @@ ERTS_INCLUDE_DIR=${ERTS_INCLUDE_DIR:-$(erl -noshell -s init stop -eval "io:forma
 CC=${CC:-cc}
 CFLAGS="-fPIC -I${ERTS_INCLUDE_DIR} -std=gnu11 \
   -Wall -Wextra -Wno-missing-field-initializers \
-  ${CFLAGS:--O3 -march=native -mtune=native -ggdb}"
+  -O3 ${CFLAGS:--march=native -mtune=native -ggdb}"
 LDFLAGS=${LDFLAGS:-}
 if [ "$(uname -s)" = Darwin ]; then
     LDFLAGS="$LDFLAGS -flat_namespace -undefined suppress"
@@ -17,7 +17,7 @@ IMPLEMENTATIONS="xorshift pcg32"
 mkdir -p $BUILD
 
 test_for_rdrand() {
-    "$CC" -o $BUILD/test_for_rdrand $SRC/test_for_rdrand.c && $BUILD/test_for_rdrand
+    "$CC" -O -o $BUILD/test_for_rdrand $SRC/test_for_rdrand.c && $BUILD/test_for_rdrand
 }
 
 if test_for_rdrand 2>/dev/null; then
