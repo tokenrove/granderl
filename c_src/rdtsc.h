ee3fac0a2ca1d460c90808537216707f82cc777f
Rémi Attab <remi@marketops.com>
2023-10-24 13:47:30 -0400

    Add native ARM64 support for rdtsc

--- c_src/rdtsc.h
+++ c_src/rdtsc.h
@@ -7,12 +7,12 @@ inline void rdtsc(uint64_t *t, uint64_t *u)
 {
     asm volatile ("rdtsc" : "=a" (*t), "=d" (*u));
 }
-#elif defined(__aarch64__) && defined(__APPLE__)
-#include <mach/mach_time.h>
-
+#elif defined(__aarch64__)
+// Userspace equivalent of rdtsc readable in user-space:
+// https://developer.arm.com/documentation/ddi0595/2021-12/AArch64-Registers/CNTVCT-EL0--Counter-timer-Virtual-Count-register
 inline void rdtsc(uint64_t *t, uint64_t *u __attribute__((unused)))
 {
-    *t = mach_absolute_time();
+    asm volatile("mrs %0, cntvct_el0" : "=r" (*t));
 }
 #else
 #error "Read your platform's perf counter here"
